a24f02422dddf38a409e8c0f32596eec618e2dd9
fatmaahmed0 <fatmaahmed3255@gmail.com>
2025-09-29 00:43:13 +0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+ $(".sideblack").hide();
+ $(".sidewhite").animate({ left:0 }, 500);
+
+
+
+// document.querySelector(".open").addEventListener("click", function (e) {
+//   e.preventDefault();
+//   $(".sideblack").animate({ width: '236px' }, 500);
+//   $(".sidewhite").animate({ left:300 }, 500);
+//   // $('.sidebar').show(500);
+// });
+
