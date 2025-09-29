e7e003cd701068fed32af4efc64f690397d47651
Ziad-Mansourr <01556746404ziad@gmail.com>
2025-09-29 16:28:49 +0300

    Refactor HTML structure and improve responsiveness across multiple files
    - Updated grid layout from 4 columns to 12 columns in Area.html, Ingredients.html, category.html, and index.html for better responsiveness.
    - Cleaned up HTML by removing redundant code and ensuring consistent formatting.
    - Adjusted CSS classes in output.css to support new grid layout and improved styling.
    - Enhanced JavaScript functionality in index.js for sidebar toggle behavior.
    - Fixed package-lock.json to reflect the correct project name.
    - Updated search.html to improve input field styling and functionality.

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,22 @@
- $(".sideblack").hide();
+$(".sideblack").hide();
  $(".sidewhite").animate({ left:0 }, 500);
 
 //npx @tailwindcss/cli -i ./src/css/input.css -o ./src/css/output.css --watch
 
-// document.querySelector(".open").addEventListener("click", function (e) {
-//   e.preventDefault();
+document.querySelector(".open").addEventListener("click", function (e) {
+  e.preventDefault();
 //   $(".sideblack").animate({ width: '236px' }, 500);
-//   $(".sidewhite").animate({ left:300 }, 500);
-//   // $('.sidebar').show(500);
-// });
+$('.sideblack').show(500);
+  $(".sidewhite").animate({ left:236 }, 500);
+  $('.open').css('display' , 'none')
+  $('.close').css('display' , 'block')
+});
+document.querySelector(".close").addEventListener("click", function (e) {
+  e.preventDefault();
+//   $(".sideblack").animate({ width: '236px' }, 500);
+$(".sidewhite").animate({ left:0 }, 500);
+$(".sideblack").hide();
+  $('.open').css('display' , 'block')
+  $('.close').css('display' , 'none')
+});
 
